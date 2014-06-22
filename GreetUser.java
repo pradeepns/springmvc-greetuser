c6fca8dfedc10f894d2c8ce3e8f52e6de0d7c912
pradeepns <mailpradeepns@gmail.com>
2014-06-22 01:18:32 -0700

    GreetUser.java
    Controller class handles requests mapped to URL "/greetuser/{username}". Returns a String view name and later it will be resolved to a specific view i.e. JSP file using UrlBasedViewResolver servlet.

---
+++ GreetUser.java
@@ -0,0 +1,19 @@
+GreetUser.java
+========================================================
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/greetUser/{username}")
+public class GreetUser {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String greetUser(@PathVariable String username, Model model) {
+		model.addAttribute("message", "Hello user " + username);
+		return "helloworld";
+	}
+}
